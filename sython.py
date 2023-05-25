3f52716093c1f880ad808a94f5b8c8c221fcaad1
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-05-25 16:49:28 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -35,7 +35,15 @@ import heroku3
 
 heroku_conn = heroku3.from_key(heroku_api_key)
 app = heroku_conn.apps()[app_name]
+sython.start()
 
+
+
+@sython.on(events.NewMessage(pattern='\.تحديث'))
+async def update(event):
+    await event.respond('جارٍ التحديث...')
+    app.builds().create(source_blob={"url": "https://github.com/sythontm/testhk"})
+    await event.respond('تم التحديث بنجاح!')
 
 @sython.on(events.NewMessage(outgoing=True, pattern=".الاوامر"))
 async def _(event):
