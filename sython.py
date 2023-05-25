0dc49cf75a38f788844b3a098db6116ce4621715
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-05-25 15:20:32 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -1,8 +1,36 @@
-import requests 
-from telethon import TelegramClient, events
-import os
-import heroku3
+from telethon.tl.functions.channels import LeaveChannelRequest
+import telethon
+from time import sleep
+from telethon import events
 from config import *
+import os
+import logging
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon import TelegramClient, events
+from collections import deque
+from telethon import functions
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions
+from hijri_converter import Gregorian
+from telethon.tl.functions.channels import LeaveChannelRequest
+import datetime
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+import requests
+import heroku3
 
 
 heroku_conn = heroku3.from_key(heroku_api_key)
