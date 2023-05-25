718ae71cc54b6e1a64cd80a69e2418f8088a3bfc
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-05-25 19:21:53 +0300

    Update sython.py

--- sython.py
+++ sython.py
@@ -1,34 +1,7 @@
-from telethon.tl.functions.channels import LeaveChannelRequest
+
 import telethon
 from time import sleep
-from telethon import events
-from config import *
-import os
-import logging
-import asyncio
-import time
-from telethon.tl import functions, types
-from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from telethon.utils import get_display_name
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.errors import FloodWaitError
-from telethon import TelegramClient, events
-from collections import deque
-from telethon import functions
-from telethon.errors.rpcerrorlist import (
-    UserAlreadyParticipantError,
-    UserNotMutualContactError,
-    UserPrivacyRestrictedError,
-)
-from telethon.tl.functions.channels import InviteToChannelRequest
-from telethon.tl.types import InputPeerUser
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl import functions
-from hijri_converter import Gregorian
-from telethon.tl.functions.channels import LeaveChannelRequest
-import datetime
-from telethon.tl.functions.messages import GetHistoryRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest
+
 import requests
 import heroku3
 
@@ -36,6 +9,14 @@ import heroku3
 
 sython.start()
 
+heroku_conn = heroku3.from_key(heroku_api_key)
+app = heroku_conn.apps()[app_name]
+
+@sython.on(events.NewMessage(pattern='\.تحديث'))
+async def update(event):
+    await event.respond('جارٍ التحديث...')
+    app.builds().create(source_blob={"url": "https://github.com/sythontm/testhk"})
+    await event.respond('تم التحديث بنجاح!')
 
 
 @sython.on(events.NewMessage(outgoing=True, pattern=".الاوامر"))
